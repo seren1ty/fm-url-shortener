b5023fb390ee5868ce03c7b658bf228efc4299ed
David Morgan <ball41@gmail.com>
2021-05-20 22:06:31 +1000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Features
+      Pricing
+      Resources
+
+      Login
+      Sign Up
+
+      More than just shorter links
+
+      Build your brand’s recognition and get detailed insights
+      on how your links are performing.
+
+      Get Started
+
+      Shorten a link here...
+
+      Shorten It!
+
+      Advanced Statistics
+
+      Track how your links are performing across the web with our
+      advanced statistics dashboard.
+
+      Brand Recognition
+
+      Boost your brand recognition with each click. Generic links don’t
+      mean a thing. Branded links help instil confidence in your content.
+
+      Detailed Records
+
+      Gain insights into who is clicking your links. Knowing when and where
+      people engage with your content helps inform better decisions.
+
+      Fully Customizable
+
+      Improve brand awareness and content discoverability through customizable
+      links, supercharging audience engagement.
+
+      Boost your links today
+
+      Get Started
+
+      Features
+
+      Link Shortening
+      Branded Links
+      Analytics
+
+      Resources
+
+      Blog
+      Developers
+      Support
+
+      Company
+
+      About
+      Our Team
+      Careers
+      Contact
+
+      <div className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>.
+        Coded by <a href="https://github.com/seren1ty">Seren1ty</a>.
+      </div>
     </div>
   );
 }
